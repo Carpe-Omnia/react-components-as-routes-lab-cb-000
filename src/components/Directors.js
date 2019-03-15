f6d7360e129d27431a67689cecbd47d499665a2d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 19:08:00 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,18 @@ const Directors = () => {
   return (
     <div>
       <h1>Directors Page</h1>
+      {directors.map(function(director){
+        return (
+          <h3>{director.name}</h3>
+          <ul>
+          {director.movies.map(function(movie){
+            return(
+              <li>{movie}</li>
+            )
+          })}
+          </ul>
+        )
+      })}
     </div>
   );
 }
