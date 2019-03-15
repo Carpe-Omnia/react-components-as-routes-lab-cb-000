f9825edb4860ae98d79070d055e76fddcde80a67
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 19:11:13 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,20 @@ const Actors = () => {
   return (
     <div>
       <h1>Actors Page</h1>
+      {actors.map(function(actor){
+        return(
+          <div>
+            {actor.name}
+            <ul>
+              {actor.movies.map(function(movie){
+                return(
+                  <li>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
