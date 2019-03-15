fff385c94c6e6ffde055cd6e6ccb136178155a50
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 18:53:08 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,20 @@ const Movies = () => {
   return (
     <div>
         <h1>Movies Page</h1>
+          {movies.map (function(movie){
+            return(
+              <div>
+                <strong> Name: {movie.title} </strong>
+                <p>Time: {movie.time}</p><br></br>
+                <ul>
+                  {movie.genres.map((genre) {
+                    return <li>{genre}</li> 
+                  })}
+                </ul>   
+                  })}
+              </div>  
+            )
+          })}
     </div>
   );
 };
