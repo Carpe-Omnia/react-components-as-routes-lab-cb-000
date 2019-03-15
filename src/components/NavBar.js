2ecd54c0661cebd7fa80a2f73cc7a4d90ff96ef1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-15 19:04:24 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,12 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      <NavLink to="/" >Home</NavLink><br></br>
-      <NavLink to="/movies" >Movies</NavLink><br></br>
-      <NavLink to="/directors" >Directors</NavLink><br></br>
-      <NavLink to="/actors" >Actors</NavLink><br></br>
+      <ul>
+        <li><NavLink to="/" >Home</NavLink></li>
+        <li><NavLink to="/movies" >Movies</NavLink></li>
+        <li><NavLink to="/directors" >Directors</NavLink></li>
+        <li><NavLink to="/actors" >Actors</NavLink></li>
+      </ul>  
     </div>
   );
 };
